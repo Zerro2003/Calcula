afa5ab11cfc027a43b3cf1239bc86ecb631f50f2
Zerro2003 <mugishaelie160@gmail.com>
2026-04-01 20:06:23 +0200

    refactor: simplify Button component to use a single value prop

--- src/comps/Button.tsx
+++ src/comps/Button.tsx
@@ -1,21 +1,3 @@
-import React from "react";
-
-type ButtonProps = {
-  label: string;
-  onClick: (label: string) => void;
-  variant?: "default" | "operator";
-};
-
-const Button: React.FC<ButtonProps> = ({
-  label,
-  onClick,
-  variant = "default",
-}) => {
-  return (
-    <button className={`btn ${variant}`} onClick={() => onClick(label)}>
-      {label}
-    </button>
-  );
-};
-
-export default Button;
+export default function Button(props: { value: string }) {
+  return <button>{props.value}</button>;
+}
