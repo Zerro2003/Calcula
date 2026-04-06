03f0f44d27d6c35dbe95fb9ab42dc4722abb6705
Zerro2003 <mugishaelie160@gmail.com>
2026-04-06 14:45:33 +0200

    feat: create Display component to render calculator output with leading zero handling

--- src/comps/Display.tsx
+++ src/comps/Display.tsx
@@ -1,7 +1,9 @@
-export default function Display(prop: { num: string }) {
+export default function Display({ num }: { num: string }) {
   return (
-    <div className="h-20 mx-[31.5rem] bg-gray-400 text-3xl text-right">
-      {prop.num}
+    <div className="h-20 bg-gray-400 text-5xl text-right">
+      {num.length > 1 && num[0] === "0" && num[1] !== "."
+        ? num.slice(1)
+        : num || "0"}
     </div>
   );
 }
